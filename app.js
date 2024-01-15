819ce263eff912c3d01233369c163a3b90badbc9
Akintola Rahmat <akintolarahmah@gmail.com>
2024-01-15 14:22:45 +0000

    add nodemon to package script

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = 5000;
+const routes = require("./src/routes/noteRouter");
 
-app.get("/", (req, res) => {
+app.use(express.json());
+
+app.get("/api", (req, res) => {
   res.send("Hello World!");
 });
 
