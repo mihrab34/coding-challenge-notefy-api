73ec210bacb61ec6f349b56569e04bc8a3c327c0
Akintola Rahmat <akintolarahmah@gmail.com>
2024-01-16 16:05:46 +0000

    update db config file

--- models/index.js
+++ models/index.js
@@ -10,7 +10,9 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.username) {
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else if (config.username) {
   sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
